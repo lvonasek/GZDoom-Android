46feee0f7be5dcf526e43b9ce2c39012a6e38f81
lubos <lubos@mapfactor.com>
2017-08-12 14:34:31 +0200

    User interface removed

--- doom/src/main/java/net/nullsum/doom/AppSettings.java
+++ doom/src/main/java/net/nullsum/doom/AppSettings.java
@@ -2,14 +2,9 @@ package net.nullsum.doom;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Environment;
 
 import com.beloko.touchcontrols.TouchSettings;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Random;
 
 public class AppSettings {
 
@@ -58,46 +53,10 @@ public class AppSettings {
         immersionMode = getBoolOption(ctx, "immersion_mode", true);
     }
 
-    public static String getQuakeFullDir()
+    public static String getFullDir()
     {
         String quakeFilesDir = AppSettings.gzdoomBaseDir;
         return quakeFilesDir + "/config";
-    }
-
-    public static void createDirectories(Context ctx)
-    {
-        boolean scan = false;
-
-        String d = "";
-        if (!new File(getQuakeFullDir() + d).exists())
-            scan = true;
-
-        new File(getQuakeFullDir() + d).mkdirs();
-
-        //This is totally stupid, need to do this so folder shows up in explorer!
-        if (scan)
-        {
-            File f = new File(getQuakeFullDir() + d , "temp_");
-            try {
-                f.createNewFile();
-                new SingleMediaScanner(ctx, false,  f.getAbsolutePath());
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-        else
-        {
-            new File(getQuakeFullDir() + d , "temp_").delete();
-        }
-    }
-
-    public static void setFloatOption(Context ctx,String name, float value)
-    {
-        SharedPreferences settings = ctx.getSharedPreferences("OPTIONS",    Context.MODE_MULTI_PROCESS);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putFloat(name, value);
-        editor.apply();
     }
 
     public static boolean getBoolOption(Context ctx,String name, boolean def)
@@ -106,40 +65,10 @@ public class AppSettings {
         return settings.getBoolean(name, def);
     }
 
-    public static void setBoolOption(Context ctx,String name, boolean value)
-    {
-        SharedPreferences settings = ctx.getSharedPreferences("OPTIONS",    Context.MODE_MULTI_PROCESS);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putBoolean(name, value);
-        editor.apply();
-    }
-
     public static int getIntOption(Context ctx,String name, int def)
     {
         SharedPreferences settings = ctx.getSharedPreferences("OPTIONS",    Context.MODE_MULTI_PROCESS);
         return settings.getInt(name, def);
-    }
-
-    public static void setIntOption(Context ctx,String name, int value)
-    {
-        SharedPreferences settings = ctx.getSharedPreferences("OPTIONS",    Context.MODE_MULTI_PROCESS);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putInt(name, value);
-        editor.apply();
-    }
-
-    public static long getLongOption(Context ctx,String name, long def)
-    {
-        SharedPreferences settings = ctx.getSharedPreferences("OPTIONS",    Context.MODE_MULTI_PROCESS);
-        return settings.getLong(name, def);
-    }
-
-    public static void setLongOption(Context ctx,String name, long value)
-    {
-        SharedPreferences settings = ctx.getSharedPreferences("OPTIONS",    Context.MODE_MULTI_PROCESS);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putLong(name, value);
-        editor.apply();
     }
 
     public static String getStringOption(Context ctx,String name, String def)
--- doom/src/main/java/net/nullsum/doom/EntryActivity.java
+++ doom/src/main/java/net/nullsum/doom/EntryActivity.java
@@ -1,215 +1,83 @@
 package net.nullsum.doom;
 
-import android.app.ActionBar;
-import android.app.ActionBar.Tab;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.app.ActivityCompat;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MotionEvent;
 import android.content.pm.PackageManager;
-import static android.Manifest.*;
 
-import net.nullsum.doom.AppSettings;
-import net.nullsum.doom.OptionsFragment;
-import net.nullsum.doom.Utils;
-import com.beloko.touchcontrols.GamePadFragment;
-
-
-public class EntryActivity extends FragmentActivity  {
-
-    GamePadFragment gamePadFrag;
-
-    /**
-     * The serialization (saved instance state) Bundle key representing the
-     * current tab position.
-     */
-    private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
-    private static final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+public class EntryActivity extends Activity
+{
+    private static final int PERMISSIONS_CODE = 1993;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (ContextCompat.checkSelfPermission(this, permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{ permission.WRITE_EXTERNAL_STORAGE }, PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
-        }
-
-        setContentView(R.layout.activity_quake);
-
-        // Set up the action bar to show tabs.
-        final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
         AppSettings.reloadSettings(getApplication());
+        setupPermissions();
+    }
 
-        GamePadFragment.gamepadActions = Utils.getGameGamepadConfig();
+    protected void setupPermissions() {
+        String[] permissions = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            boolean ok = true;
+            for (String s : permissions)
+                if (checkSelfPermission(s) != PackageManager.PERMISSION_GRANTED)
+                    ok = false;
 
-        actionBar.addTab(actionBar.newTab().setText("Gzdoom").setTabListener(new TabListener<LaunchFragmentGZdoom>(this, "Gzdoom", LaunchFragmentGZdoom.class)));
-        actionBar.addTab(actionBar.newTab().setText("gamepad").setTabListener(new TabListener<GamePadFragment>(this, "gamepad", GamePadFragment.class)));
-        actionBar.addTab(actionBar.newTab().setText("options").setTabListener(new TabListener<OptionsFragment>(this, "options", OptionsFragment.class)));
-
-
-        String last_tab = AppSettings.getStringOption(getApplicationContext(), "last_tab", "");
-        actionBar.setSelectedNavigationItem(0);
-
-        gamePadFrag = (GamePadFragment)getFragmentManager().findFragmentByTag("gamepad");
-
+            if (!ok)
+                requestPermissions(permissions, PERMISSIONS_CODE);
+            else
+                onRequestPermissionsResult(PERMISSIONS_CODE, null, new int[]{PackageManager.PERMISSION_GRANTED});
+        } else
+            onRequestPermissionsResult(PERMISSIONS_CODE, null, new int[]{PackageManager.PERMISSION_GRANTED});
     }
 
     @Override
-    public void onRestoreInstanceState(Bundle savedInstanceState) {
-        // Restore the previously serialized current tab position.
-        if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
-            getActionBar().setSelectedNavigationItem(
-                    savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
-        }
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        // Serialize the current tab position.
-        outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getActionBar()
-                .getSelectedNavigationIndex());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.activity_quake, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onGenericMotionEvent(MotionEvent event) {
-        if (gamePadFrag == null)
-            gamePadFrag = (GamePadFragment)getFragmentManager().findFragmentByTag("gamepad");
-
-        return gamePadFrag.onGenericMotionEvent(event);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
     {
-        if (gamePadFrag == null)
-            gamePadFrag = (GamePadFragment)getFragmentManager().findFragmentByTag("gamepad");
-
-        if (gamePadFrag.onKeyDown(keyCode, event))
-            return true;
-        else
-            return super.onKeyDown(keyCode, event);
-    }
-
-    @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event)
-    {
-        if (gamePadFrag == null)
-            gamePadFrag = (GamePadFragment)getFragmentManager().findFragmentByTag("gamepad");
-
-        if ( gamePadFrag.onKeyUp(keyCode, event))
-            return true;
-        else
-            return super.onKeyUp(keyCode, event);
-    }
-
-    public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
-        private final FragmentActivity mActivity;
-        private final String mTag;
-        private final Class<T> mClass;
-        private final Bundle mArgs;
-        private Fragment mFragment;
-
-        public TabListener(FragmentActivity activity, String tag, Class<T> clz) {
-            this(activity, tag, clz, null);
-        }
-
-        public TabListener(FragmentActivity activity, String tag, Class<T> clz, Bundle args) {
-            mActivity = activity;
-            mTag = tag;
-            mClass = clz;
-            mArgs = args;
-
-            // Check to see if we already have a fragment for this tab, probably
-            // from a previously saved state.  If so, deactivate it, because our
-            // initial state is that a tab isn't shown.
-            mFragment = mActivity.getFragmentManager().findFragmentByTag(mTag);
-
-            if (mFragment == null) //Actually create all fragments NOW
+        switch (requestCode)
+        {
+            case PERMISSIONS_CODE:
             {
-                mFragment = Fragment.instantiate(mActivity, mClass.getName(), mArgs);
-                FragmentTransaction ft =  mActivity.getFragmentManager().beginTransaction();
-                ft.add(android.R.id.content, mFragment, mTag);
-                ft.commit();
-            }
-
-
-            //if (mFragment != null && !mFragment.isDetached()) {
-            if (mFragment != null && !mFragment.isHidden()) {
-                FragmentTransaction ft = mActivity.getFragmentManager().beginTransaction();
-                //ft.detach(mFragment);
-                ft.hide(mFragment);
-                ft.commit();
-            }
-        }
-
-        public void onTabSelected(Tab tab, FragmentTransaction ft) {
-            if (mFragment == null) {
-                mFragment = Fragment.instantiate(mActivity, mClass.getName(), mArgs);
-                ft.add(android.R.id.content, mFragment, mTag);
-            } else {
-                //ft.attach(mFragment);
-                //ft.setCustomAnimations(R., R.anim.fade_out, R.anim.fade_in, R.anim.fade_out);
-                ft.show(mFragment);
-            }
-        }
-
-        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-            if (mFragment != null) {
-                //ft.detach(mFragment);
-                ft.hide(mFragment);
-            }
-        }
-
-        public void onTabReselected(Tab tab, FragmentTransaction ft) {
-            //Toast.makeText(mActivity, "Reselected!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-//    public  boolean isStoragePermissionGranted() {
-//        if (Build.VERSION.SDK_INT >= 23) {
-//            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                    == PackageManager.PERMISSION_GRANTED) {
-//                //Log.v(TAG,"Permission is granted");
-//                return true;
-//            } else {
-//
-//                //Log.v(TAG,"Permission is revoked");
-//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-//                return false;
-//            }
-//        }
-//        else { //permission is automatically granted on sdk<23 upon installation
-//            //Log.v(TAG,"Permission is granted");
-//            return true;
-//        }
-//    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // dirty hack :(
-                    final ActionBar actionBar = getActionBar();
-                    actionBar.setSelectedNavigationItem(1);
-                    actionBar.setSelectedNavigationItem(0);
-                }
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    startGame(AppSettings.getFullDir(),"-iwad doom1.wad ");
+                finish();
+                break;
             }
         }
     }
 
+
+    private void startGame(final String base,String args)
+    {
+        //Extract data
+        Utils.copyAsset(this,"gzdoom.pk3",base);
+        Utils.copyAsset(this,"gzdoom.sf2",base);
+        Utils.copyAsset(this,"doom1.wad",base);
+
+        AppSettings.setStringOption(this, "last_tab", "gzdoom");
+        Intent intent = new Intent(this, net.nullsum.doom.Game.class);
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        int resDiv = AppSettings.getIntOption(this,  "gzdoom_res_div", 1);
+        intent.putExtra("res_div",resDiv);
+
+        intent.putExtra("game_path",base);
+        intent.putExtra("game", "doom");
+
+        String saveDir;
+        saveDir = " -savedir " + base + "/gzdoom_saves";
+
+        String fluidSynthFile = "gzdoom.sf2";
+
+        intent.putExtra("args",args + saveDir + " +set fluid_patchset " + fluidSynthFile + " +set midi_dmxgus 0 ");
+
+        startActivity(intent);
+    }
 }
